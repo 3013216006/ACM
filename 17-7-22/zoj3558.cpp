1985f1b0c69db51dffd3b6d08fa15acfc586c2ae
3013216006 <fuxuzhou@tju.edu.cn>
2017-07-23 00:01:26 +0800

    7-22

---
+++ 17-7-22/zoj3558.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	while(~scanf("%d",&n)){
+		long long ans=0;
+		for(int i=1;i*i<=n;i++){
+			long long tp=(n-1)/i;
+			
+
+		}
+	}
+}
